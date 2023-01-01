1725cb8d0f4272f831a7285969695434ea23900a
John Doe <johndoe@email.com>
2023-01-01 05:28:57 +0400

    add fiture

--- js/animations.js
+++ js/animations.js
@@ -21,3 +21,30 @@ function increaseNumberAnimationStep(i, element, endNumber) {
     increaseNumberAnimationStep(0, element, 5000); 
   }
   initIncreaseNumberAnimation();
+
+
+    document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+      // Должны добавить еще одно текстовое поле
+      let formContainer = document.createElement("div");
+      formContainer.classList.add("form__group", "form__other-input");
+
+      const input = document.createElement("input");
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text";
+      window.in
+
+      formContainer.appendChild(input);
+
+      document.querySelector(".form form").insertBefore(formContainer, document.querySelector(".form__submit"));
+    }
+    
+    const otherInput = document.querySelector('form__other-input');
+    if (event.target.value !== 'other' && Boolean(otherInput)) {
+      // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+      document.querySelector(".form form").removeChild(otherInput);
+    }
+  });
+
+
+
